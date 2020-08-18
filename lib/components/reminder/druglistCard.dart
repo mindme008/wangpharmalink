dd1c94d5ac841d2aacbe4d70f1cb03585a16c2e2
mac <Chayanit008@gmail.com>
2020-08-18 17:46:18 +0700

    อัป ข้อมูลยา

--- lib/components/reminder/druglistCard.dart
+++ lib/components/reminder/druglistCard.dart
@@ -4,50 +4,40 @@ import 'package:flutter_demo/models/Druglist.dart';
 import 'package:flutter_demo/size.config.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class DruglistCard extends StatelessWidget{
+class DruglistCard extends StatelessWidget {
+
+  var druglist;
+  DruglistCard({Key key, this.druglist}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+
     double defaultSize = SizeConfig.defaultSize;
     // TODO: implement build
     return AspectRatio(
-      aspectRatio: 2.3,
+      aspectRatio: 0.8,
       child: Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(defaultSize *1.8),
         ),
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  children: <Widget>[
-                    //Text(
-                      //druglist[0].drug,
-                      //style: TextStyle(
-                        //fontSize: defaultSize *2.0,
-                        //color: Colors.white,
-                        //fontWeight: FontWeight.bold,
-                      //),
-                      //maxLines: 2,
-                      //overflow: TextOverflow.ellipsis,
-                    //),
-                    SizedBox(height: defaultSize *0.5,),
+        child: ListView.builder(
+          itemBuilder: (context, int index){
+            return ListTile(
+              contentPadding: EdgeInsets.fromLTRB(10, 1, 10, 1),
+              onTap: (){
+              },
+              leading: Image.asset('assets/images/pill.png', fit: BoxFit.cover, width: 60, height: 60,),
+              title: Text('${druglist[index].drugName}', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold), overflow: TextOverflow.ellipsis),
+              subtitle:Text('ครั้งละ ${druglist[index].drugDose} ${druglist[index].drugUnitdose} ${druglist[index].drugOrder}'),
+              trailing: IconButton(
+                  icon: Icon(Icons.alarm, color: Colors.orangeAccent, size: 40,),
+                  onPressed: (){
 
-                  ],
-                ),
+                  }
               ),
-            ),
-            SizedBox(width: defaultSize *0.5,),
-            AspectRatio(
-              aspectRatio: 0.71,
-              //child: SvgPicture.asset(
-                //druglist[0].imageSrc,
-                //fit: BoxFit.cover,
-               // alignment: Alignment.centerLeft,
-              ),
-            //)
-          ],
+            );
+          },
+          itemCount: druglist != null ? druglist.length : 0,
         ),
       ),
     );
