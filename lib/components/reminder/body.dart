6635faf0fd9fd30b86949eb44059c0bcd82db766
mac <Chayanit008@gmail.com>
2020-08-20 19:33:54 +0700

    อัป showdialog

--- lib/components/reminder/body.dart
+++ lib/components/reminder/body.dart
@@ -4,6 +4,7 @@ import 'package:flutter_demo/components/reminder/categories.dart';
 import 'package:flutter_demo/components/reminder/druglistCard.dart';
 import 'package:flutter_demo/constants.dart';
 import 'package:flutter_demo/models/Druglist.dart';
+import 'package:flutter_demo/size.config.dart';
 
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
@@ -45,8 +46,8 @@ class BodyState extends State<Body>{
         print('Connect ERROR');
       }
     }
-
   }
+
 
 
   @override
@@ -65,25 +66,66 @@ class BodyState extends State<Body>{
           children: <Widget>[
             Categories(),
             Container(
-              alignment: Alignment.topRight,
-              child: new FloatingActionButton(child: new Icon(Icons.calendar_today), heroTag: 2,
-                onPressed: () {
-                  showDatePicker(
-                      context: context, initialDate: _dateTime == null ? DateTime.now() : _dateTime,
-                      firstDate: DateTime(2001),
-                      lastDate: DateTime(2025)
-                  ).then((date) {
-                    setState(() {
-                      _dateTime = date;
+              decoration: BoxDecoration(
+                boxShadow:[BoxShadow(
+                  color: Colors.orangeAccent.withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 9,
+                  offset: Offset(0, 1),
+                ),
+                ],
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: FlatButton(
+                  onPressed: () {
+                    showDatePicker(
+                        context: context, initialDate: _dateTime == null ? DateTime.now() : _dateTime,
+                        firstDate: DateTime(2001),
+                        lastDate: DateTime(2025)
+                    ).then((date) {
+                      setState(() {
+                        _dateTime = date;
+                      });
                     });
-                  });
-                },
-                backgroundColor: Colors.orange,
+                  },
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                  color: Colors.orange,
+                  splashColor: Colors.black,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.calendar_today,
+                        color: Colors.white,
+                        size: 25,
+                      ),
+                      Text(" Select Date"),
+                      SizedBox(width: 10),
+                      Text(_dateTime == null ? '' : _dateTime.toString()),
+                    ],
+                  ),
+                ),
               ),
             ),
-            Text(_dateTime == null ? '' : _dateTime.toString()),
+            //Container(
+              //alignment: Alignment.topRight,
+              //child: new FloatingActionButton(child: new Icon(Icons.calendar_today), heroTag: 2,
+                //onPressed: () {
+                  //showDatePicker(
+                    //  context: context, initialDate: _dateTime == null ? DateTime.now() : _dateTime,
+                      //firstDate: DateTime(2001),
+                      //lastDate: DateTime(2025)
+                  //).then((date) {
+                    //setState(() {
+                      //_dateTime = date;
+                    //});
+                  //});
+                //},
+                //backgroundColor: Colors.orange,
+              //),
+            //),
+            //Text(_dateTime == null ? '' : _dateTime.toString()),
             DruglistCard(druglist:druglistAll),
-
 
             //Text("รายละเอียดยา",
             //style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
@@ -99,3 +141,76 @@ class BodyState extends State<Body>{
     );
   }
 }
+
+class DruglistCard extends StatelessWidget {
+
+  var druglist;
+
+  DruglistCard({Key key, this.druglist }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double defaultSize = SizeConfig.defaultSize;
+    // TODO: implement build
+    return AspectRatio(
+      aspectRatio: 0.8,
+      child: ListView.builder(
+        itemBuilder: (context, int index) {
+          return Container(
+            margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(20)),
+            child: ListTile(
+              contentPadding: EdgeInsets.fromLTRB(10, 1, 10, 1),
+              onTap: () {
+                showDialog(context: context,
+                    builder: (_) => new AlertDialog(
+                      title: new Text('แจ้งเตือนรับประทานยาวันที่ :${druglist[index].drugStart}',style: TextStyle(color: kPrimaryColor)),
+                      content: new Text('${druglist[index].drugName} ครั้งละ ${druglist[index]
+                          .drugDose} ${druglist[index].drugUnitdose} ${druglist[index].drugIndication} '
+                          '${druglist[index].drugDescription}'),
+                      actions: <Widget>[
+                        FlatButton(
+                          child: Text("OK",style: TextStyle(color: kPrimaryColor),),
+                          onPressed: (){ Navigator.of(context).pop();
+                          },
+                        ),
+                      ],
+                    )
+                );
+              },
+              //leading: Image.asset('assets/images/pill.png', fit: BoxFit.cover, width: 60, height: 60,),
+              //leading: Text('${druglist[index].drugTime1}',
+              //  style: TextStyle(color: kPrimaryColor)),
+              leading: Container(
+                padding: EdgeInsets.only(right: 12.0),
+                decoration: new BoxDecoration(
+                    border: new Border(
+                        right: new BorderSide(
+                            width: 1.0, color: Colors.orange))),
+                child: Text('${druglist[index].drugAlert}',
+                    style: TextStyle(color: kPrimaryColor)),
+              ),
+              title: Text('วันที่ ${druglist[index].drugStart}',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  overflow: TextOverflow.ellipsis),
+              subtitle: Text(
+                  '${druglist[index].drugName} ครั้งละ ${druglist[index]
+                      .drugDose} ${druglist[index].drugUnitdose}'),
+              trailing: IconButton(
+                  icon: Icon(
+                    Icons.alarm, color: Colors.orangeAccent, size: 30,),
+                  onPressed: () {
+                  }
+              ),
+            ),
+          );
+        },
+        itemCount: druglist != null ? druglist.length : 0,
+      ),
+    );
+  }
+}
+
+
+
